a21a583f0ee055900e55e9bc6182d08e964950f3
mauudev <mtrigo143@gmail.com>
2023-10-26 20:02:38 -0400

    integrate with main App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import PDF from "./features/pdf-builder/pdf";
+
+const report = {
+  title: "My app here",
+  entries: [
+    {
+      item: {
+        assetPreviewLarge: "/public/favicon.ico",
+      },
+      pageBreak: false,
+      notes:
+        "Este es el texto enriquecido que se mostrará en el componente RichText",
+    },
+  ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <PDF report={report} />;
 }
 
 export default App;
