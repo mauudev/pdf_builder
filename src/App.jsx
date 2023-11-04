58827960de7fc9c28e6ed847955e19f72e9bed08
mauudev <mtrigo143@gmail.com>
2023-11-04 00:14:16 -0400

    fixed spacing between components and wysiwyg editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import WYSIWYGEditor from "./features/pdf-builder/text-editor/editor";
+// import WYSIWYGEditor from "./features/pdf-builder/text-editor/editor";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">Rich Text Editor</header>
+      <header className="app-header">
+        <div className="header-content">
+          <img className="logo" src="cat.jpeg" alt="Logo" />
+          <span>Rich Text Editor</span>
+        </div>
+      </header>
       <div className="app-container">
         <div className="sidebar"></div>
         <div className="editor-container">
-          <p style={{ fontSize: "18px", color: "red" }}>HOLA ESTA ES UNA PRUEBA PARA VER SOLAPAMIENTOS DE TEXTO</p>
           {/* <WYSIWYGEditor /> */}
         </div>
       </div>
