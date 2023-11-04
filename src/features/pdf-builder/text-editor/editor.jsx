e6cd6c6b57fd9745b959f291218f856f3c0bdd0a
mauudev <mtrigo143@gmail.com>
2023-11-04 14:10:00 -0400

    (WIP)fixed text margin overflows

--- src/features/pdf-builder/text-editor/editor.jsx
+++ src/features/pdf-builder/text-editor/editor.jsx
@@ -10,7 +10,10 @@ const WYSIWYGEditor = () => {
   );
   const [pageSize, setPageSize] = useState("carta");
   const [fontSize, setFontSize] = useState(12);
-  const [margin, setMargin] = useState(20);
+  const [marginTop, setMarginTop] = useState(20);
+  const [marginLeft, setMarginLeft] = useState(20);
+  const [marginRight, setMarginRight] = useState(20);
+  const [marginBottom, setMarginBottom] = useState(20);
 
   const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
@@ -25,19 +28,39 @@ const WYSIWYGEditor = () => {
     }
   };
 
-  const changeMargin = (newMargin) => {
-    setMargin(parseInt(newMargin)); // Convertir a número y establecerlo en el estado
+  const changeMarginTop = (newMargin) => {
+    setMarginTop(parseInt(newMargin));
+  };
+  const changeMarginLeft = (newMargin) => {
+    setMarginLeft(parseInt(newMargin));
+  };
+  const changeMarginRight = (newMargin) => {
+    setMarginRight(parseInt(newMargin));
+  };
+  const changeMarginBottom = (newMargin) => {
+    setMarginBottom(parseInt(newMargin));
   };
 
   const pageStyles = {
     width: pageSize === "carta" ? "210mm" : "216mm",
     height: pageSize === "carta" ? "297mm" : "356mm",
     backgroundColor: "white",
-    padding: `${margin}px`, // Usar el valor de margen del estado
     border: "1px solid #000",
     display: "flex",
     justifyContent: "flex-start",
     alignItems: "flex-start",
+  };
+
+  const contentStyles = {
+    fontSize: `${fontSize}px`,
+    width: `calc(100% - ${marginLeft + marginRight}px)`,
+    marginLeft: `${marginLeft}px`,
+    marginRight: `${marginRight}px`,
+    marginTop: `${marginTop}px`,
+    marginBottom: `${marginBottom}px`,
+    textAlign: "left",
+    overflowWrap: "break-word",
+    border: "5px solid #000",
   };
 
   const editorContent = convertToRaw(editorState.getCurrentContent());
@@ -65,24 +88,48 @@ const WYSIWYGEditor = () => {
         </select>
       </div>
       <div id="margin-selector">
-        <label htmlFor="page-margin">Configura el margen:</label>
-        <input
-          type="number"
-          id="page-margin"
-          value={margin}
-          onChange={(e) => changeMargin(e.target.value)}
-        />
+        <div className="margin-top">
+          <label htmlFor="page-margin_top">Arriba:</label>
+          <input
+            type="number"
+            id="page-margin_top"
+            value={marginTop}
+            onChange={(e) => changeMarginTop(e.target.value)}
+          />
+        </div>
+        <div className="margin-left">
+          <label htmlFor="page-margin_left">Izquierda:</label>
+          <input
+            type="number"
+            id="page-margin_left"
+            value={marginLeft}
+            onChange={(e) => changeMarginLeft(e.target.value)}
+          />
+        </div>
+        <div className="margin-right">
+          <label htmlFor="page-margin_right">Derecha:</label>
+          <input
+            type="number"
+            id="page-margin_right"
+            value={marginRight}
+            onChange={(e) => changeMarginRight(e.target.value)}
+          />
+        </div>
+        <div className="margin-bottom">
+          <label htmlFor="page-margin_bottom">Abajo:</label>
+          <input
+            type="number"
+            id="page-margin_bottom"
+            value={marginBottom}
+            onChange={(e) => changeMarginBottom(e.target.value)}
+          />
+        </div>
       </div>
-      <div style={pageStyles}>
-        <div
-          style={{
-            fontSize: `${fontSize}px`,
-            width: "100%",
-            textAlign: "left",
-            padding: "1rem",
-          }}
-        >
-          {editorContent.blocks.map((block) => block.text).join("\n")}
+      <div className="preview">
+        <div style={pageStyles}>
+          <div style={contentStyles}>
+            {editorContent.blocks.map((block) => block.text).join("\n")}
+          </div>
         </div>
       </div>
     </div>
