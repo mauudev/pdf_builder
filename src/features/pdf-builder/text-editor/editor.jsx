391bb07365a7df2b0623eb1e716927833b81e50b
mauudev <mtrigo143@gmail.com>
2023-11-06 19:51:05 -0400

    refactored editor component

--- src/features/pdf-builder/text-editor/editor.jsx
+++ src/features/pdf-builder/text-editor/editor.jsx
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
 import DOMPurify from "dompurify";
-import { convertToHTML } from "draft-convert";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./editor.css";
 
 const WYSIWYGEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const [pageSize, setPageSize] = useState("carta");
   const [fontSize, setFontSize] = useState(12);
-  const [marginTop, setMarginTop] = useState(20);
-  const [marginLeft, setMarginLeft] = useState(20);
-  const [marginRight, setMarginRight] = useState(20);
-  const [marginBottom, setMarginBottom] = useState(20);
+  const [marginValues, setMarginValues] = useState({
+    top: 20,
+    left: 20,
+    right: 20,
+    bottom: 20,
+  });
   const [convertedContent, setConvertedContent] = useState(null);
 
   useEffect(() => {
-    let html = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+    const html = draftToHtml(convertToRaw(editorState.getCurrentContent()));
     setConvertedContent(html);
   }, [editorState]);
 
@@ -28,33 +27,21 @@ const WYSIWYGEditor = () => {
     setEditorState(editorState);
   };
 
-  const changePageSize = (size) => {
+  const handleChangePageSize = (size) => {
     setPageSize(size);
-    if (size === "carta") {
-      setFontSize(12);
-    } else if (size === "oficio") {
-      setFontSize(14);
-    }
+    setFontSize(size === "carta" ? 12 : 14);
   };
 
-  const changeMarginTop = (newMargin) => {
-    setMarginTop(parseInt(newMargin));
-  };
-  const changeMarginLeft = (newMargin) => {
-    setMarginLeft(parseInt(newMargin));
-  };
-  const changeMarginRight = (newMargin) => {
-    setMarginRight(parseInt(newMargin));
-  };
-  const changeMarginBottom = (newMargin) => {
-    setMarginBottom(parseInt(newMargin));
+  const handleMarginChange = (marginKey, value) => {
+    setMarginValues((prevValues) => ({
+      ...prevValues,
+      [marginKey]: parseInt(value),
+    }));
   };
 
-  const createMarkup = (html) => {
-    return {
-      __html: DOMPurify.sanitize(html),
-    };
-  };
+  const createMarkup = (html) => ({
+    __html: DOMPurify.sanitize(html),
+  });
 
   const pageStyles = {
     width: pageSize === "carta" ? "210mm" : "216mm",
@@ -68,20 +55,15 @@ const WYSIWYGEditor = () => {
 
   const contentStyles = {
     fontSize: `${fontSize}px`,
-    width: `calc(100% - ${marginLeft + marginRight}px)`,
-    marginLeft: `${marginLeft}px`,
-    marginRight: `${marginRight}px`,
-    marginTop: `${marginTop}px`,
-    marginBottom: `${marginBottom}px`,
+    width: `calc(100% - ${marginValues.left + marginValues.right}px)`,
+    marginLeft: `${marginValues.left}px`,
+    marginRight: `${marginValues.right}px`,
+    marginTop: `${marginValues.top}px`,
+    marginBottom: `${marginValues.bottom}px`,
     textAlign: "left",
     overflowWrap: "break-word",
-    border: "5px solid #000",
   };
 
-  const editorContent = draftToHtml(
-    convertToRaw(editorState.getCurrentContent())
-  );
-  console.log(`EDITOR CONTENT: ${JSON.stringify(editorContent)}`);
   return (
     <div className="editor-container">
       <div className="editor">
@@ -98,49 +80,26 @@ const WYSIWYGEditor = () => {
         <select
           id="page-size"
           value={pageSize}
-          onChange={(e) => changePageSize(e.target.value)}
+          onChange={(e) => handleChangePageSize(e.target.value)}
         >
           <option value="carta">Carta</option>
           <option value="oficio">Oficio</option>
         </select>
       </div>
       <div id="margin-selector">
-        <div className="margin-top">
-          <label htmlFor="page-margin_top">Arriba:</label>
-          <input
-            type="number"
-            id="page-margin_top"
-            value={marginTop}
-            onChange={(e) => changeMarginTop(e.target.value)}
-          />
-        </div>
-        <div className="margin-left">
-          <label htmlFor="page-margin_left">Izquierda:</label>
-          <input
-            type="number"
-            id="page-margin_left"
-            value={marginLeft}
-            onChange={(e) => changeMarginLeft(e.target.value)}
-          />
-        </div>
-        <div className="margin-right">
-          <label htmlFor="page-margin_right">Derecha:</label>
-          <input
-            type="number"
-            id="page-margin_right"
-            value={marginRight}
-            onChange={(e) => changeMarginRight(e.target.value)}
-          />
-        </div>
-        <div className="margin-bottom">
-          <label htmlFor="page-margin_bottom">Abajo:</label>
-          <input
-            type="number"
-            id="page-margin_bottom"
-            value={marginBottom}
-            onChange={(e) => changeMarginBottom(e.target.value)}
-          />
-        </div>
+        {["top", "left", "right", "bottom"].map((marginKey) => (
+          <div key={marginKey} className={`margin-${marginKey}`}>
+            <label htmlFor={`page-margin_${marginKey}`}>
+              {capitalizeFirstLetter(marginKey)}:
+            </label>
+            <input
+              type="number"
+              id={`page-margin_${marginKey}`}
+              value={marginValues[marginKey]}
+              onChange={(e) => handleMarginChange(marginKey, e.target.value)}
+            />
+          </div>
+        ))}
       </div>
       <div className="preview">
         <div style={pageStyles}>
@@ -153,5 +112,9 @@ const WYSIWYGEditor = () => {
     </div>
   );
 };
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default WYSIWYGEditor;
