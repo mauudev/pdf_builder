692daac96007e8b2f6d15003649999ceb06b32e6
mauudev <mtrigo143@gmail.com>
2023-11-13 22:46:36 -0400

    moving files and cleanup

--- src/utils/pdf-drawfter/rich-text/paragraph/paragraph.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text } from "@react-pdf/renderer";
-
-export const Paragraph = ({styles, children }) => {
-  <View style={...styles}>
-    <Text>{children}</Text>
-  </View>;
-};
