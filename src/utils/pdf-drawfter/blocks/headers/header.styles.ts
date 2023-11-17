b9a1dfeb81b0c8c7926f33645f0b4f923a29dfeb
mauudev <mtrigo143@gmail.com>
2023-11-16 22:20:01 -0400

    add header block and builders

---
+++ src/utils/pdf-drawfter/blocks/headers/header.styles.ts
@@ -0,0 +1,26 @@
+export const headerStyles = {
+  "header-one": {
+    fontSize: 24,
+    fontWeight: 800,
+  },
+  "header-two": {
+    fontSize: 22,
+    fontWeight: 700,
+  },
+  "header-three": {
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  "header-four": {
+    fontSize: 18,
+    fontWeight: 500,
+  },
+  "header-five": {
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  "header-six": {
+    fontSize: 14,
+    fontWeight: 300,
+  },
+};
---
+++ src/utils/pdf-drawfter/builders/headers-builder.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from "react";
+import { View } from "@react-pdf/renderer";
+import { IBuilder, IBlock, RawJSON, StyleMap } from "../contracts";
+import { parseViewStyle } from "../utils";
+import HeaderBlock from "../blocks/headers/headers";
+
+/**
+ * Builder de componentes de tipo 'header'
+ */
+class HeaderBlockBuilder implements IBuilder {
+  private blockComponent: IBlock | undefined;
+  public styleMap: StyleMap;
+
+  constructor(styleMap: StyleMap) {
+    this.styleMap = styleMap;
+    this.blockComponent = new HeaderBlock(styleMap);
+  }
+
+  public getBlockComponent(): IBlock | undefined {
+    return this.blockComponent;
+  }
+
+  public getBuiltBlock(rawJson: RawJSON): ReactElement | undefined {
+    const headerTypes = [
+      "header-one",
+      "header-two",
+      "header-three",
+      "header-four",
+      "header-five",
+      "header-six",
+    ];
+    if (rawJson.type && !headerTypes.includes(rawJson.type)) {
+      console.error(`type ${rawJson.type} not supported`);
+      return;
+    }
+    const block = this.buildHeaderBlock(rawJson);
+    this.getBlockComponent()?.reset();
+    return block;
+  }
+
+  public buildHeaderBlock(rawJson: RawJSON): ReactElement | undefined {
+    const blockStyle = parseViewStyle(rawJson.data, this.styleMap);
+    return (
+      <View key={rawJson.key} style={blockStyle}>
+        {this.blockComponent?.getComponent(rawJson)}
+      </View>
+    );
+  }
+}
+
+export default HeaderBlockBuilder;
