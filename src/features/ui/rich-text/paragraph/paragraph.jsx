d8369f456dccb65e75c3c22f1cfaa31766f34b16
mauudev <mtrigo143@gmail.com>
2023-11-20 21:34:50 -0400

    moved editor component files and cleanup

--- src/features/ui/rich-text/paragraph/paragraph.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { View, Text } from "@react-pdf/renderer";
-import { styles } from "./paragraph.styles";
-
-export const Paragraph = ({ children }) => {
-  <View>
-    <Text style={styles.text}>{children}</Text>
-  </View>;
-};
