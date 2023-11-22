b260e22d09c00ce34fdfd9e1305dc4f4a1d3ccf6
mauudev <mtrigo143@gmail.com>
2023-11-21 22:36:48 -0400

    add custom logger

---
+++ src/features/pdf-builder/logger.ts
@@ -0,0 +1,17 @@
+class Logger {
+  static log(message: string): void {
+    if (process.env.NODE_ENV === "development") {
+      console.log(message);
+    }
+  }
+
+  static warning(message: string): void {
+    console.warn(message);
+  }
+
+  static error(error: string): void {
+    console.error(error);
+  }
+}
+
+export default Logger;
