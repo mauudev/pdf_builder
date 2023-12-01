89f6515b262f9d0eb928cbf1ce42a3634300bf93
mauudev <mtrigo143@gmail.com>
2023-11-30 23:21:08 -0400

    refactor editor and integrate table modal option

--- src/features/text-editor/editor.jsx
+++ src/features/text-editor/editor.jsx
@@ -1,19 +1,114 @@
-import React, { useEffect } from 'react';
-import { convertToRaw } from 'draft-js';
+import React, { useEffect, useState } from 'react';
+import { convertToRaw, ContentState, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
 import DOMPurify from 'dompurify';
 import { Editor } from 'react-draft-wysiwyg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-free';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { styles } from './editor.styles';
 import PreviewModal from '../ui/modal/preview-modal';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import { capitalizeFirstLetter, parsePointValue } from '../../utils/helpers';
 import PDFBuilder from '../pdf-builder/pdf-builder';
 import { useEditor } from './contexts/editor-context';
+import TableModal from '../ui/modal/table-modal';
+import Logger from '../pdf-builder/logger';
+import { css } from 'glamor';
 
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Logger from '../pdf-builder/logger';
 
+library.add(far, fas, fab);
+
+function customChunkRenderer(nodeName, node) {
+  const allowedNodes = ['div', 'table', 'tbody', 'tr', 'th', 'td', 'thead', 'style'];
+
+  if (allowedNodes.includes(nodeName)) {
+    return {
+      type: nodeName.toString().toUpperCase(),
+      mutability: 'MUTABLE',
+      data: {
+        // Pass whatever you want here (like id, or classList, etc.)
+        innerText: node.innerText,
+        innerHTML: node.innerHTML,
+      },
+    };
+  }
+  return null;
+}
+
+function entityMapper(entity) {
+  if (entity.type === 'DIV') {
+    return `<div>${entity.data.innerHTML}</div>`;
+  }
+  if (entity.type === 'TABLE') {
+    return `<table>${entity.data.innerHTML}</table>`;
+  }
+  if (entity.type === 'TBODY') {
+    return `<tbody>${entity.data.innerHTML}</tbody>`;
+  }
+  if (entity.type === 'TR') {
+    return `<tr>${entity.data.innerHTML}</tr>`;
+  }
+  if (entity.type === 'TH') {
+    return `<th>${entity.data.innerHTML}</th>`;
+  }
+  if (entity.type === 'TD') {
+    return `<td>${entity.data.innerHTML}</td>`;
+  }
+  if (entity.type === 'STYLE') {
+    return `<style>${entity.data.innerHTML}</style>`;
+  }
+  return '';
+}
+
+function entityMapperToComponent(entity) {
+  if (entity.type === 'DIV') {
+    return () => <div dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'TABLE') {
+    return () => <table dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'TBODY') {
+    return <tbody dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'TR') {
+    return () => <tr dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'TH') {
+    return () => <th dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'TD') {
+    return () => <td dangerouslySetInnerHTML={{ __html: entity.data.innerHTML }} />;
+  }
+  if (entity.type === 'STYLE') {
+    return () => <style>{entity.data.innerHTML}</style>;
+  }
+
+  return '';
+}
+
+function customBlockRenderFunc(block, config) {
+  console.log('block', block.getLength());
+  console.log('config', config);
+  if (block.getType() === 'atomic') {
+    const contentState = config.getEditorState().getCurrentContent();
+    const entity = contentState.getEntity(block.getEntityAt(0));
+    return {
+      component: entityMapperToComponent(entity),
+      editable: false,
+      props: {
+        children: () => entity.innerHTML,
+      },
+    };
+  }
+}
 const WYSIWYGEditor = () => {
   const { editorState, dispatch } = useEditor();
+  const [isTableModalOpen, setIsTableModalOpen] = useState(false);
   const pdfBuilder = new PDFBuilder(editorState.editor.rawContent.blocks);
 
   useEffect(() => {
@@ -25,7 +120,7 @@ const WYSIWYGEditor = () => {
       type: 'SET_EDITOR_STATE',
       payload: {
         editorState: newEditorState,
-        convertedContent: draftToHtml(convertToRaw(newEditorState.getCurrentContent())),
+        convertedContent: draftToHtml(convertToRaw(newEditorState.getCurrentContent()), null, false, entityMapper),
         rawContent: convertToRaw(newEditorState.getCurrentContent()),
       },
     });
@@ -64,23 +159,41 @@ const WYSIWYGEditor = () => {
     return pdfBuilder.PDFPreview(pdfStyles, styles.modalPreview);
   };
 
-  const parsePointValue = (value) => {
-    const numericChars = [...value].filter((char) => !isNaN(char) || char === '.');
-    return parseFloat(numericChars.join(''));
+  const handleTableModalOpen = () => setIsTableModalOpen(true);
+  const handleTableModalClose = () => setIsTableModalOpen(false);
+
+  const handleSaveTable = (tableData) => {
+    const contentBlock = htmlToDraft(tableData.html, customChunkRenderer);
+    let newEditorState;
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+      newEditorState = EditorState.createWithContent(contentState);
+    } else {
+      newEditorState = EditorState.createEmpty();
+    }
+    onEditorStateChange(newEditorState);
   };
+
+  const AddTableOption = () => (
+    <div className="rdw-option-wrapper" onClick={handleTableModalOpen}>
+      <FontAwesomeIcon title="Add Table" icon="fa-solid fa-table" />
+    </div>
+  );
 
   return (
     <div style={styles.editorLayout}>
       <div style={styles.wrapper}>
         <Editor
           editorState={editorState.editor.state}
-          wrapperClassName="wrapper-class"
-          editorClassName="editor-class"
-          toolbarClassName="toolbar-class"
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
           onEditorStateChange={onEditorStateChange}
+          customBlockRenderFunc={customBlockRenderFunc}
           toolbar={{
             options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'history', 'remove', 'colorPicker'],
           }}
+          toolbarCustomButtons={[<AddTableOption />]}
         />
         <div style={styles.gridContainer}>
           <div style={styles.gridItem}>
@@ -119,7 +232,7 @@ const WYSIWYGEditor = () => {
           </div>
         </div>
       </div>
-      {/* <TableModal isOpen={isTableModalOpen} onClose={() => setTableModalOpen(false)} onSave={handleSaveTable} /> */}
+      <TableModal isOpen={isTableModalOpen} onClose={handleTableModalClose} onSave={handleSaveTable} />
       <PreviewModal pdfPreview={buildPdfPreview()} />
       <div style={styles.livePreview}>
         <div style={editorState.pageStyles.pageSize === 'LETTER' ? styles.cartaPreview : styles.oficioPreview}>
