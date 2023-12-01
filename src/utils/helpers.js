1494b59b67a0c5080b558ce82e982722d4f7c535
mauudev <mtrigo143@gmail.com>
2023-11-30 23:19:26 -0400

    move helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
 export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+export const parsePointValue = (value) => {
+  const numericChars = [...value].filter((char) => !isNaN(char) || char === '.');
+  return parseFloat(numericChars.join(''));
+};
